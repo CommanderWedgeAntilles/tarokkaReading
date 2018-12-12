f813da6e72c396f13f8be2c7db10b706c84dbbc1
CommanderWedgeAntilles <fli12003@byui.edu>
2018-12-12 01:49:10 -0700

    image fix

--- public/javascripts/readCards-clientside.js
+++ public/javascripts/readCards-clientside.js
@@ -169,16 +169,16 @@ function hideContent(){
 
 function displayLowCards(a,b,c){
 
-    $("#tome").append("<img src='", + a, + ".png'>");
-    $("#symbol").append("<img src='", + b, + ".png'>");
-    $("#sword").append("<img src='", + c, + ".png'>");
+    $("#tome").append("<img src='./images/", + a, + ".png'>");
+    $("#symbol").append("<img src='./images/", + b, + ".png'>");
+    $("#sword").append("<img src='./images/", + c, + ".png'>");
 }
 
 function displayHighCards(a,b){
     filename = filenameFix(a);
-    $("#enemy").append("<img src='", + filename, + ".png'>");
+    $("#enemy").append("<img src='./images/", + filename, + ".png'>");
     filename = filenameFix(b);
-    $("#location").append("<img src='", + filename, + ".png'>");
+    $("#location").append("<img src='./images/", + filename, + ".png'>");
 }
 
 function reveal(){
