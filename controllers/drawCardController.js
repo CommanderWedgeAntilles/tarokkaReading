a89777a15816a5efcdd94b8d8eb42d8c7e574526
CommanderWedgeAntilles <fli12003@byui.edu>
2018-12-13 00:43:23 -0700

    database queries results added controller work

--- controllers/drawCardController.js
+++ controllers/drawCardController.js
@@ -3,30 +3,33 @@ const drawCardModels = require("../models/drawCardModels.js")
 function getLowCardA(req,res){
     //get one low card from the deck
     console.log("Getting requested low card");
-    var results = drawCardModels.getLowCardByID(id,1,function(results){res.json(results);});
+    var id = req.query.id;
+    drawCardModels.getLowCardByID(id,1,function(error, results){res.json(results);});
     
   }
   function getLowCardB(req,res){
     //get one low card from the deck
     console.log("Getting requested low card");
-    var results = drawCardModels.getLowCardByID(id,2,function(results){res.json(results);});
+    var id = req.query.id;
+    drawCardModels.getLowCardByID(id,2,function(error, results){res.json(results);});
   }
   function getLowCardC(req,res){
     //get one low card from the deck
     console.log("Getting requested low card");
-    var results = drawCardModels.getLowCardByID(id,3,function(results){res.json(results);});
+    var id = req.query.id;
+    drawCardModels.getLowCardByID(id,3,function(error, results){res.json(results);});
   }
 
   function getHighCardA(req,res){
     //get one highcard from the deck
     var id = req.query.id;
-    var results = drawCardModels.getHighCardByID(id,1,function(results){res.json(results);});
+    drawCardModels.getHighCardByID(id,1,function(error, results){res.json(results);});
   }
 
   function getHighCardB(req,res){
     //get one highcard from the deck
     var id = req.query.id;
-    var results = drawCardModels.getHighCardByID(id,2,function(results){res.json(results);});
+    drawCardModels.getHighCardByID(id,2,function(error, results){res.json(results);});
   }
 
 
