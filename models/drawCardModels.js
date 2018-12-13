8534c1697359f302e57754e229c2f1e8d72b6ec5
CommanderWedgeAntilles <fli12003@byui.edu>
2018-12-12 23:39:44 -0700

    database queries

--- models/drawCardModels.js
+++ models/drawCardModels.js
@@ -5,32 +5,81 @@ const pool = new Pool({connectiionString: db_url});
 
 //Still working on
 function getLowPlotByID(id,callback){
-    var results;
-    callback(results);
+    var sql ="SELECT id, plotName, plotReading FROM lowPlotPoints WHERE id=$1::number";
+    var params = [id];
+    pool.query(sql, params, function(err,db_results){
+        if(err){
+            throw err;
+        }else {
+            //data here
+            console.log(db_results);
+            
+            var results;
+            callback(null, results);
+        }
+    });
+
 }
 
 function getHighPlotByID(id,callback){
-    var results;
-    callback(results);
+    var sql ="SELECT id, plotName, plotReading FROM highPlotPoints WHERE id=$1::number";
+    var params = [id];
+    pool.query(sql, params, function(err,db_results){
+        if(err){
+            throw err;
+        }else {
+            //data here
+            console.log(db_results);
+            
+            var results;
+            callback(null, results);
+        }
+    });
 }
 
 function getLowCardByID(id,plotID,callback){
     var plot = getLowPlotByID(plotID,function(results){res.json(results);});
-    var results = {
-        fortune: [
 
-        ]
-    };
-    callback(results);
+    var sql ="SELECT id, cardName, cardReading FROM lowCards WHERE id=$1::number";
+    var params = [id];
+    pool.query(sql, params, function(err,db_results){
+        if(err){
+            throw err;
+        }else {
+            //data here
+            console.log(db_results);
+            
+            var results = {
+                fortune: [
+        
+                ]
+            };
+
+            callback(null, results);
+        }
+    });
 }
+
 function getHighCardByID(id,plotID,callback){
     var plot = getHighPlotByID(plotID,function(results){res.json(results);});
-    var results ={
-        fortune: [
-        
-        ]
-    };
-    callback(results);
+    
+    var sql ="SELECT id, cardName FROM highCards WHERE id=$1::number";
+    var params = [id];
+    pool.query(sql, params, function(err,db_results){
+        if(err){
+            throw err;
+        }else {
+            //data here
+            console.log(db_results);
+    
+            var results ={
+                fortune: [
+                
+                ]
+            };
+            callback(null, results);
+        }
+    });
 
 }
 
@@ -40,4 +89,3 @@ module.exports ={
     getLowCardByID: getLowCardByID,
     getHighCardByID: getHighCardByID
 }
-
