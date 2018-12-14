51ace462dc1565e1818c2fb3614f65fa6c8ff0ca
CommanderWedgeAntilles <fli12003@byui.edu>
2018-12-13 17:19:17 -0700

    clientside console

--- models/drawCardModels.js
+++ models/drawCardModels.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg");
-const db_url =process.env.DATABASE_URL;
+const db_url = process.env.DATABASE_URL;
+console.log(db_url);
 
-const pool = new Pool({connectiionString: db_url});
+const pool = new Pool({connectionString: db_url});
 
 
 
